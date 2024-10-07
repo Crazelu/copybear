2c9f9e718e7ba2d3838c364199244b44df0819ac
Crazelu <eberelucky64@gmail.com>
2024-10-07 19:09:25 +0100

    Initial Commit

---
+++ CopyBear/CopyBearApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyBearApp.swift
+//  CopyBear
+//
+//  Created by LUCKY EBERE on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CopyBearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CopyBearTests/CopyBearTests.swift
@@ -0,0 +1,17 @@
+//
+//  CopyBearTests.swift
+//  CopyBearTests
+//
+//  Created by LUCKY EBERE on 07/10/2024.
+//
+
+import Testing
+@testable import CopyBear
+
+struct CopyBearTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
